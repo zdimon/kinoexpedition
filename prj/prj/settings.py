78865b1ddce5543a92cc30d06e4bea883befe619
zdimon <zdimon@example.com>
2019-07-09 14:41:44 +0300

    auto commit

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -135,6 +135,8 @@ TINYMCE_DEFAULT_CONFIG = {
     'theme': "advanced",
     'cleanup_on_startup': True,
     'custom_undo_redo_levels': 10,
+    'width': '100%',
+    'height': 300
 }
 TINYMCE_SPELLCHECKER = True
 TINYMCE_COMPRESSOR = True
