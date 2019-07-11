a5f635f5b0f66ec7099fd4c1673c20b3d9721ab7
zdimon <zdimon@example.com>
2019-07-11 10:19:32 +0300

    auto commit

--- prj/main/views.py
+++ prj/main/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from .models import Slider, Page, Gallery
 def home(req):
     slider = Slider.objects.all().order_by('-id')
-    homepage = Page.objects.get(alias='home')
-    schedulepage = Page.objects.get(alias='schedule')
+    #homepage = Page.objects.get(alias='home')
+    #schedulepage = Page.objects.get(alias='schedule')
     pages = Page.objects.all()
     gallery = Gallery.objects.all()
     cntx = {
         'slider': slider, 
-        'homepage': homepage,
+        #'homepage': homepage,
         'pages': pages,
-        'schedulepage': schedulepage,
+        #'schedulepage': schedulepage,
         'gallery': gallery
         }
     return render(req,'index.html', cntx)
