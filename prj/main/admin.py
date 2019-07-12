018a7fc73174fe6c3b5c8e56989fc6a2768dd522
zdimon <zdimon@example.com>
2019-07-12 15:37:57 +0300

    auto commit

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -4,7 +4,7 @@ from image_cropping import ImageCroppingMixin
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ['title', 'alias']
-    list_editable = ('order',)
+    #list_editable = ('order',)
 
 admin.site.register(Page, PageAdmin)
 
